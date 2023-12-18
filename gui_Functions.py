815d2bab2f20d8c5e84f019836d47206035fbbb4
Nitesh3927 <96771005+Nitesh3927@users.noreply.github.com>
2023-12-18 20:38:46 +0530

    Date time issue fixeed(@ash)
    how are u smart@ash

--- gui_Functions.py
+++ gui_Functions.py
@@ -11,18 +11,6 @@ def update_time_date_labels(time_label, date_label, root):
     date_label.config(text="Date: " + current_date)
     root.after(1000, lambda: update_time_date_labels(time_label, date_label, root))
 
-
-# def time_date(root, time_label,date_label):
-#     update_time_date_labels()
-
-
-# def update_time_date_labels():
-#     current_time = datetime.now().strftime("%H:%M:%S")
-#     current_date = datetime.now().strftime("%d - %m - %Y")
-#     time_label.config(text="Time: " + current_time)
-#     date_label.config(text="Date: " + current_date)
-#     root.after(1000, update_time_date_labels)
-
 def subject_selected(showingSubject, Sub_OptionMenu__Label):
     # showingSubject = sub.get()
     Sub_OptionMenu__Label.config(text="Subject: " + showingSubject)
